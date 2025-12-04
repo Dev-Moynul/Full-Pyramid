9f44ae2c04898e5dd46b93d14f0bf698a13c5339
Dev-Moynul <stu.moynulislam@gmail.com>
2025-12-04 14:58:29 +0600

    Full pyramid in py

--- fullPyramid.py
+++ fullPyramid.py
@@ -1,10 +1,4 @@
-# Input
-N = int(input("Enter the size of the pyramid: "))
-
-# Loop through each level of the pyramid
-for i in range(1, N + 1):
-    # Print spaces to center the stars
-    print(' ' * (N - i), end='')
-    
-    # Print the stars with a space between them
+n = int(input("Enter the size of the pyramid: "))
+for i in range(1, n + 1):
+    print(' ' * (n - i), end='')
     print('* ' * i)
